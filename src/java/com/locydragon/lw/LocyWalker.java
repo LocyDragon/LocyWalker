0cbf908dc87495410de3732fce65cdf23ace056f
LocyDragon <2424441676@qq.com>
2018-07-18 18:27:53 +0800

    default

--- src/java/com/locydragon/lw/LocyWalker.java
+++ src/java/com/locydragon/lw/LocyWalker.java
@@ -3,5 +3,8 @@ package com.locydragon.lw;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LocyWalker extends JavaPlugin {
+	@Override
+	public void onEnable(){
 
+	}
 }
---
+++ src/java/com/locydragon/lw/effect/PacketEffect.java
@@ -0,0 +1,36 @@
+package com.locydragon.lw.effect;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.PacketContainer;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import java.util.Random;
+import java.util.Set;
+
+public class PacketEffect {
+	public static final ProtocolManager manager = ProtocolLibrary.getProtocolManager();
+	public static void sendPacketEffect(EffectTypeEnum type, Player who, Location where) {
+		PacketContainer container = manager.createPacket(PacketType.Play.Server.WORLD_PARTICLES);
+		container.getIntegers().write(0, type.getF());
+		container.getIntegers().write(1, 10);
+		container.getBooleans().write(0, false);
+		container.getFloat().write(0, (float)where.getX());
+		container.getFloat().write(1, (float)where.getY());
+		container.getFloat().write(2, (float)where.getZ());
+		container.getFloat().write(3, (float)(where.getX() + new Random().nextGaussian()));
+		container.getFloat().write(4, (float)(where.getY() + new Random().nextGaussian()));
+		container.getFloat().write(5, (float)(where.getZ() + new Random().nextGaussian()));
+		container.getIntegerArrays().write(0, new int[]{});
+		try {
+			manager.sendServerPacket(who, container);
+		} catch (Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+	public static void sendPacketEffect(EffectTypeEnum type, Player who, Set<Location> where) {
+		where.stream().forEach(loc -> sendPacketEffect(type, who, loc));
+	}
+}
